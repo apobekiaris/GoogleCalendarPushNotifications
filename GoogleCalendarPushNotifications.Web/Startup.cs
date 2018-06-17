fb089100994c8781aeecae23e516cdde89ca2674
mattwebber <mattwebber93@outlook.com>
2018-06-17 09:46:52 +0100

    Allow hosting of static files.

--- GoogleCalendarPushNotifications.Web/Startup.cs
+++ GoogleCalendarPushNotifications.Web/Startup.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace GoogleCalendarPushNotifications.Web
 {
@@ -35,6 +29,7 @@ namespace GoogleCalendarPushNotifications.Web
             }
 
             app.UseMvc();
+            app.UseStaticFiles();
         }
     }
 }
